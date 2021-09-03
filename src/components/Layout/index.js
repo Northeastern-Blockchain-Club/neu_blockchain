573b4930ef1e551ee57c39226e23d7947b53dc3e
Nolan <braman.n@northeastern.edu>
2021-09-03 00:51:55 -0400

    added layout, child wrapping, style imports, and scss imports with theme

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,28 +1,45 @@
 
 
 import React, { Component } from 'react'
+
 // imported 3rd party libraries
 import { Helmet } from 'react-helmet'
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from "@material-ui/core/Container"
 
 // Custom components and hooks
+import Header from "../Header/index"
 import useSiteMetadata from '../Hooks/useMetadata.js'
 
 // styles
+import theme from "../../Theme/theme.js"
 import "../../styles/main.scss"
-
+import "@fontsource/roboto"
+// styles
+const pageStyles = {
+    color: "#232129",
+    padding: "20px 10px",
+}
 
 const Layout = ({ children }) => {
     const { title, description } = useSiteMetadata()
     return (
-        <div>
+        <ThemeProvider theme={theme}>
             <Helmet>
                 <title>{title}</title>
                 <meta name="description" content={description} />
             </Helmet>
+            <CssBaseline />
             <div>
-                {children}
+                <Header />
+                <main style={pageStyles}>
+                    <Container>
+                        {children}
+                    </Container>
+                </main>
             </div>
-        </div>
+        </ThemeProvider >
     )
 }
 
