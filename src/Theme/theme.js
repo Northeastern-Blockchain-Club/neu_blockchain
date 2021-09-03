ecccdfa620797d1590d025b1d7f36091be21bb80
Nolan <braman.n@northeastern.edu>
2021-09-03 00:52:16 -0400

    added material-ui Theme

---
+++ src/Theme/theme.js
@@ -0,0 +1,36 @@
+import { createTheme } from '@material-ui/core/styles';
+
+const theme = createTheme({
+    palette: {
+        type: 'light',
+        primary: {
+            main: '#4282d4',
+        },
+        secondary: {
+            main: '#f50057',
+        },
+        typography: {
+            fontFamily: [
+                '-apple-system',
+                'BlinkMacSystemFont',
+                '"Segoe UI"',
+                'Roboto',
+                '"Helvetica Neue"',
+                'Arial',
+                'sans-serif',
+                '"Apple Color Emoji"',
+                '"Segoe UI Emoji"',
+                '"Segoe UI Symbol"',
+            ].join(','),
+        },
+        // Used by `getContrastText()` to maximize the contrast between
+        // the background and the text.
+        contrastThreshold: 3,
+        // Used by the functions below to shift a color's luminance by approximately
+        // two indexes within its tonal palette.
+        // E.g., shift from Red 500 to Red 300 or Red 700.
+        tonalOffset: 0.2,
+    }
+
+});
+export default theme;
