c341e438290a3d88a8d7ed25b4f2638f50e38814
Nolan <nolan.braman@gmail.com>
2021-09-03 23:19:12 -0400

    fix for icon flash

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,73 +1,68 @@
-import React, { Component } from 'react';
-import {Link} from "gatsby"
+import React, { Component } from "react";
+import { Link } from "gatsby";
 
-import AppBar from '@material-ui/core/AppBar';
-import Button from '@material-ui/core/Button';
+import AppBar from "@material-ui/core/AppBar";
+import Button from "@material-ui/core/Button";
 import Container from "@material-ui/core/Container";
 import Toolbar from "@material-ui/core/Toolbar";
 
-
 import IconButton from "@material-ui/core/IconButton";
-import InstagramIcon from '@material-ui/icons/Instagram';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import InstagramIcon from "@material-ui/icons/Instagram";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
 
 import "./index.scss";
 
-
 export class Header extends Component {
-    render() {
-        return (
-            <header className="header">
-                <AppBar color="primary">
-                    <Container>
+  render() {
+    return (
+      <header className="header">
+        <AppBar color="primary">
+          <Container>
+            <Toolbar>
+              <Button color="inherit" component={Link} to="/">
+                Home
+              </Button>
+              {/* Add instagram, contact, and join button */}
+              <Button color="inherit" component={Link} to="/about">
+                Who We Are
+              </Button>
+              <Button color="inherit" component={Link} to="/join">
+                Join
+              </Button>
+              <Button color="inherit" component={Link} to="/events">
+                Events
+              </Button>
+              <Button color="inherit" component={Link} to="/contact">
+                Contact
+              </Button>
+              <div className="grow" />
 
-                    <Toolbar>
-                        <Button color="inherit" component={Link} to="/" >
-                            Home
-                        </Button>
-                        {/* Add instagram, contact, and join button */}
-                        <Button color="inherit" component={Link} to="/about">
-                            Who We Are
-                        </Button>
-                        <Button color="inherit" component={Link} to="/join">
-                            Join
-                        </Button>
-                        <Button color="inherit" component={Link} to="/events">
-                            Events
-                        </Button>
-                        <Button color="inherit" component={Link} to="/contact">
-                            Contact
-                        </Button>
-                        <div className="grow" />
-
-                        <IconButton
-                            edge="end"
-                            aria-label="Instagram account"
-                            aria-controls="primary-search-instagram"
-                            aria-haspopup="true"
-                            color="inherit"
-                            href="https://www.instagram.com/neublockchain/"
-
-                        >
-                            <InstagramIcon />
-                        </IconButton>
-                        <IconButton
-                            edge="end"
-                            aria-label="Linkedin account"
-                            aria-controls="primary-search-linkedin"
-                            aria-haspopup="true"
-                            color="inherit"
-                            href="https://www.linkedin.com/company/neu-blockchain-org/"
-                        >
-                            <LinkedInIcon />
-                        </IconButton>
-
-                        </Toolbar>
-                    </Container>
-                </AppBar>
-            </header>
-        )
-    }
+              <IconButton
+                edge="end"
+                aria-label="Instagram account"
+                aria-controls="primary-search-instagram"
+                aria-haspopup="true"
+                color="inherit"
+                href="https://www.instagram.com/neublockchain/"
+              >
+                <InstagramIcon fontSize="medium" />
+              </IconButton>
+              <IconButton
+                edge="end"
+                aria-label="Linkedin account"
+                aria-controls="primary-search-linkedin"
+                aria-haspopup="true"
+                color="inherit"
+                href="https://www.linkedin.com/company/neu-blockchain-org/"
+              >
+                <LinkedInIcon fontSize="medium" />
+              </IconButton>
+            </Toolbar>
+          </Container>
+        </AppBar>
+      </header>
+    );
+  }
 }
 
-export default Header
+export default Header;
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,25 +1,27 @@
-import * as React from "react"
-import Layout from "../components/Layout"
+import * as React from "react";
+import Layout from "../components/Layout";
 
-import { Typography } from "@material-ui/core"
-import Grid from "@material-ui/core/Grid"
+import { Typography } from "@material-ui/core";
+import Grid from "@material-ui/core/Grid";
 
-import CardWithContent from "../components/CardWithContent"
-
+import CardWithContent from "../components/CardWithContent";
 
 const ContactPage = () => {
   return (
     <Layout>
-        <Grid
-          container
-          direction="column"
-          justifyContent="center"
-          alignItems="center"
-        >
-       
+      <Grid
+        container
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <CardWithContent
+          title={"There's nothing here..."}
+          body={""}
+        ></CardWithContent>
       </Grid>
     </Layout>
-  )
-}
+  );
+};
 
-export default ContactPage
+export default ContactPage;
