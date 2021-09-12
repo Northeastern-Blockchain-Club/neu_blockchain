9c1cb1227ea4340836226f1eeef5d6c5d81bc546
Nolan <nolan.braman@gmail.com>
2021-09-12 13:39:25 -0400

    updated 404 page to now have a link back go home

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,7 +2,6 @@ import * as React from "react";
 import { Link } from "gatsby";
 import Layout from "../components/Layout";
 
-import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
 
 import { Typography } from "@material-ui/core";
